eeab9f5da2fc448d540e73b8c207cacdff222cd6
Toshiki <munatotti.works@gmail.com>
2021-08-18 06:32:33 +0000

    Laravel 10 提出課題

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>PHP/Laravel 10　課題4の提出画面</title>
+    </head>
+    <body>
+        <h1>PHP/Laravel 10　課題4の提出画面</h1>
+    </body>
+</html>
