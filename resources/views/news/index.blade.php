1edfd2bd537bfbb651abdc325aa1246c1818f352
Toshiki <munatotti.works@gmail.com>
2021-09-23 08:42:47 +0000

    test

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
     <div class="container">
-        <hr color="#c0c0c0">
+        <hr color="#ffff00">
         @if (!is_null($headline))
             <div class="row">
                 <div class="headline col-md-10 mx-auto">
                     <div class="row">
                         <div class="col-md-6">
                             <div class="caption mx-auto">
-                                <div class="image">
-                                    @if ($headline->image_path)
-                                        <img src="{{ asset('storage/image/' . $headline->image_path) }}">
-                                    @endif
-                                </div>
+                               <div class="image">
+                                  @if ($headline->image_path)
+                                  <img src="{{ $headline->image_path }}">
+                                  @endif
+                               </div>
                                 <div class="title p-2">
                                     <h1>{{ str_limit($headline->title, 70) }}</h1>
                                 </div>
@@ -26,7 +26,7 @@
                 </div>
             </div>
         @endif
-        <hr color="#c0c0c0">
+        <hr color="#ffff00">
         <div class="row">
             <div class="posts col-md-8 mx-auto mt-3">
                 @foreach($posts as $post)
@@ -44,13 +44,14 @@
                                 </div>
                             </div>
                             <div class="image col-md-6 text-right mt-4">
-                                @if ($post->image_path)
-                                    <img src="{{ asset('storage/image/' . $post->image_path) }}">
-                                @endif
+                              @if ($post->image_path)
+                                <img src="{{ $post->image_path }}">
+                              @endif
                             </div>
+
                         </div>
                     </div>
-                    <hr color="#c0c0c0">
+                    <hr color="#ffff00">
                 @endforeach
             </div>
         </div>
